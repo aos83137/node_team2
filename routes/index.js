1fbae3a8fbbf49e68e5d74366fc273a48e5374ec
YongSeok <qazwsx1455@gmail.com>
2019-05-27 17:30:52 +0900

    commit 0.0.2

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('HF');
+});
+
+router.get('/signIn', function(req, res, next) {
+  res.render('signIn');
+});
+
+router.get('/registor', function(req, res, next) {
+  res.render('registor');
+});
+
+router.get('/QnA', function(req, res, next) {
+  res.render('QnA');
 });
 
 module.exports = router;

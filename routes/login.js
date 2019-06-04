44ea584c47452a202f17a0b39150d34670eaad72
YongSeok <qazwsx1455@gmail.com>
2019-06-04 19:28:44 +0900

    영준꺼 커ㅁㅅ

--- routes/login.js
+++ routes/login.js
@@ -1,34 +1,9 @@
-const express = require('express');
-const passport = require('passport');
-const bcrypt = require('bcrypt');
-const {isNotLoggedIn, isLoggedIn} = require('./middlewares');
-// const { User } = require('../models');
-
-const router = express.Router();
+var express = require('express');
+var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('login');
-});
-
-router.post('/login', isNotLoggedIn, (req, res, next) => {
-  passport.authenticate('local', (authError, user, info) => {
-    if (authError) {
-      console.error(authError);
-      return next(authError);
-    }
-    if (!user) {
-      req.flash('loginError', info.message);
-      return res.redirect('/');
-    }
-    return req.login(user, (loginError) => {
-      if (loginError) {
-        console.error(loginError);
-        return next(loginError);
-      }
-      return res.redirect('/');
-    });
-  })(req, res, next); // 미들웨어 내의 미들웨어에는 (req, res, next)를 붙입니다.
 });
 
 module.exports = router;

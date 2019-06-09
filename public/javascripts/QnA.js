0eefd576d4c914f06a3a67d42cca85a7efdff8e4
YongSeok <qazwsx1455@gmail.com>
2019-06-09 12:07:14 +0900

    와ㅏㅏㅏㅏ 성공;

--- public/javascripts/QnA.js
+++ public/javascripts/QnA.js
@@ -2,7 +2,7 @@ var QnA_btn = document.getElementById('QnA_btn');
 var backBtn = document.getElementById("backBtn");
 var question = document.getElementById("question");
 var qna_con_btn = document.getElementById("qna_con_btn");
-var u_nickName = document.getElementById("get_u_nickName");;
+var user_u_nickName = document.getElementById("get_u_nickName");
 
 backBtn.addEventListener('click', function () {
     question.style.display = 'none';
@@ -23,8 +23,7 @@ document.querySelectorAll('#QnA_table tr').forEach(function (el) {
 });
 
 // 댓글 로딩
-function getContent(id) { 
-
+function getContent(id) {
     fetch('/QnA/' + id, {
             method: 'GET'
         })
@@ -36,144 +35,77 @@ function getContent(id) {
             console.log(resJson);
             const content = resJson;
             var for_info_div = document.querySelector('#qna_information');
-            var for_info_div_normal = document.querySelector('#qna_information_normal');
-            var for_info_div_none = document.querySelector('#qna_information_none');
-
+            for_info_div.innerHTML = '';
             content.map((qna) => {
-                if (for_info_div) {
-                    for_info_div.innerHTML = '';
-
-                    var for_title = document.createElement('input');
-                    for_title.type = 'text';
-                    for_title.id = 'name'
-                    for_title.value = qna.q_title;
-                    for_title.textContent = qna.q_title;
-                    for_info_div.append(for_title);
-                    var for_content = document.createElement('textarea')
-                    for_content.value = qna.q_body;
-                    for_content.rows = '6';
-                    for_info_div.append(for_content);
-                    var edit = document.createElement('text');
-                    edit.textContent = '수정';
-                    edit.id = "edit";
-                    edit.addEventListener('click', () => {
-                        fetch('/QnA/' + qna.id, {
-                            headers: {
-                                'Accept': 'application/json',
-                                'Content-Type': 'application/json'
-                            },
-                            method: "PATCH",
-                            body: JSON.stringify({
-                                title: for_title.value,
-                                content: for_content.value
-                            })
-                        }).then((response) => {
-                            if (response.status == '200') {
-                                return response.json();
-                            }
-                        }).then((resJson) => {
-                            console.log(resJson);
-                            location.reload();
-                        }).catch((error) => {
-                            console.error('fetch 호출에서 에러발생: ' + error.message);
+                var for_title = document.createElement('input');
+                for_title.type = 'text';
+                for_title.id = 'name'
+                for_title.value = qna.q_title;
+                for_title.textContent = qna.q_title;
+                for_info_div.append(for_title);
+                var for_content = document.createElement('textarea')
+                for_content.value = qna.q_body;
+                for_content.rows = '6';
+                for_info_div.append(for_content);
+                if (user_u_nickName.value == '') {
+                    var need_login = document.createElement('text');
+                    need_login.textContent = '로그인해주세요';
+                    need_login.id = "need_login";
+                    for_info_div.append(need_login);
+                } else {
+                    if (user_u_nickName.value == qna.q_nick) {
+                        var edit = document.createElement('text');
+                        edit.textContent = '수정';
+                        edit.id = "edit";
+                        edit.addEventListener('click', () => {
+                            fetch('/QnA/' + qna.id, {
+                                headers: {
+                                    'Accept': 'application/json',
+                                    'Content-Type': 'application/json'
+                                },
+                                method: "PATCH",
+                                body: JSON.stringify({
+                                    title: for_title.value,
+                                    content: for_content.value
+                                })
+                            }).then((response) => {
+                                if (response.status == '200') {
+                                    return response.json();
+                                }
+                            }).then((resJson) => {
+                                console.log(resJson);
+                                location.reload();
+                            }).catch((error) => {
+                                console.error('fetch 호출에서 에러발생: ' + error.message);
+                            });
                         });
-                    });
-                    var remove = document.createElement('text');
-                    remove.textContent = '삭제';
-                    remove.id = "remove";
-                    remove.addEventListener('click', () => {
-                        //삭제 클릭시
-                        fetch('/QnA/' + qna.id, {
-                            method: 'delete'
-                        }).then((response) => {
-                            if (response.status == '200') {
-                                return response.json();
-                            }
-                        }).then((resJson) => {
-                            console.log(resJson);
-                            location.reload();
-                        }).catch((error) => {
-                            console.error('fetch 호출에서 에러발생: ' + error.message);
+                        var remove = document.createElement('text');
+                        remove.textContent = '삭제';
+                        remove.id = "remove";
+                        remove.addEventListener('click', () => {
+                            //삭제 클릭시
+                            fetch('/QnA/' + qna.id, {
+                                method: 'delete'
+                            }).then((response) => {
+                                if (response.status == '200') {
+                                    return response.json();
+                                }
+                            }).then((resJson) => {
+                                console.log(resJson);
+                                location.reload();
+                            }).catch((error) => {
+                                console.error('fetch 호출에서 에러발생: ' + error.message);
+                            });
                         });
-                    });
-                    for_info_div.append(edit);
-                    for_info_div.append(remove);
-                }else if(for_info_div_normal){
-                    for_info_div_normal.innerHTML = '';
-
-                    var for_title = document.createElement('input');
-                    for_title.type = 'text';
-                    for_title.id = 'name'
-                    for_title.value = qna.q_title;
-                    for_title.textContent = qna.q_title;
-                    for_title.disabled = 'disabled';
-                    for_info_div_normal.append(for_title);
-                    var for_content = document.createElement('textarea')
-                    for_content.value = qna.q_body;
-                    for_content.rows = '6';
-                    for_content.disabled = 'disabled';
-                    for_info_div_normal.append(for_content);
-                    var edit = document.createElement('text');
-                    edit.textContent = '수정';
-                    edit.id = "edit";
-                    edit.addEventListener('click', () => {
-                        fetch('/QnA/' + qna.id, {
-                            headers: {
-                                'Accept': 'application/json',
-                                'Content-Type': 'application/json'
-                            },
-                            method: "PATCH",
-                            body: JSON.stringify({
-                                title: for_title.value,
-                                content: for_content.value
-                            })
-                        }).then((response) => {
-                            if (response.status == '200') {
-                                return response.json();
-                            }
-                        }).then((resJson) => {
-                            console.log(resJson);
-                            location.reload();
-                        }).catch((error) => {
-                            console.error('fetch 호출에서 에러발생: ' + error.message);
-                        });
-                    });
-                    var remove = document.createElement('text');
-                    remove.textContent = '삭제';
-                    remove.id = "remove";
-                    remove.addEventListener('click', () => {
-                        //삭제 클릭시
-                        fetch('/QnA/' + qna.id, {
-                            method: 'delete'
-                        }).then((response) => {
-                            if (response.status == '200') {
-                                return response.json();
-                            }
-                        }).then((resJson) => {
-                            console.log(resJson);
-                            location.reload();
-                        }).catch((error) => {
-                            console.error('fetch 호출에서 에러발생: ' + error.message);
-                        });
-                    });
-                    for_info_div_normal.append(edit);
-                    for_info_div_normal.append(remove);
-                }else if(for_info_div_none){
-                    for_info_div_none.innerHTML = '';
-
-                    var for_title = document.createElement('input');
-                    for_title.type = 'text';
-                    for_title.id = 'name'
-                    for_title.value = qna.q_title;
-                    for_title.textContent = qna.q_title;
-                    for_title.disabled='disabled';
-                    for_info_div_none.append(for_title);
-                    var for_content = document.createElement('textarea')
-                    for_content.value = qna.q_body;
-                    for_content.rows = '6';
-                    for_content.disabled='disabled';
-                    for_info_div_none.append(for_content);
-                    var edit = document.createElement('text');
+                        for_info_div.append(edit);
+                        for_info_div.append(remove);
+                    } 
+                    if (user_u_nickName.value == '관리자') {
+                        var answer = document.createElement('text');
+                        answer.textContent = '답글달기';
+                        answer.id = "answer";
+                        for_info_div.append(answer);
+                    }   
                 }
             });
         }).catch((error) => {
